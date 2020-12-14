9c6a93b3a8725ba719258c205a366ac0349a5458
eabayev20 <eabayev20@gmail.com>
2020-12-13 23:53:33 -0500

    Add done done

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -5,13 +5,23 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList (int startingCapacity) {
     super(startingCapacity);
   }
-  public int findIndex(T newVal) {
+  public int findIndex(T ne) {
     for (int i = 0; i < size(); i++) {
-      if (newVal.compareTo(this.get(i)) < 0) {
+      if (ne.compareTo(this.get(i)) < 0) {
         return i;
       }
     }
     return size();
+  }
+  public void add(int index, T ne) {
+    int index1 = findIndex(ne);
+    super.add(index1, ne);
+
+  }
+  public boolean add(T ne) {
+    int index = findIndex(ne);
+    super.add(index, ne);
+    return this.get(index) == ne;
   }
 
 
