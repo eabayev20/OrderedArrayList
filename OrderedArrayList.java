6b00142a552520bab4066af7bb93ae16940cdd46
eabayev20 <eabayev20@gmail.com>
2020-12-13 23:49:22 -0500

    Find Index done

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -5,5 +5,15 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList (int startingCapacity) {
     super(startingCapacity);
   }
-  
+  public int findIndex(T newVal) {
+    for (int i = 0; i < size(); i++) {
+      if (newVal.compareTo(this.get(i)) < 0) {
+        return i;
+      }
+    }
+    return size();
+  }
+
+
+
 }
