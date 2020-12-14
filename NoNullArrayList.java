779208e0809f73ec96ed30f007c7c11f6dbe8771
eabayev20 <eabayev20@gmail.com>
2020-12-13 23:28:12 -0500

    Add done and tested

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,5 +1,20 @@
 import java.util.ArrayList;
 public class NoNullArrayList,T> extends ArrayList<T> {
 
+  public void add(int index, T element) {
+    try {
+      if (element == null) {
+        throw new IllegalArgumentException("Null, Error");
+      }
+      super.add(index, element);
+    }
+    catch (IllegalArgumentException e) {
+      throw new IllegalArgumentException ("Null, Error");
+    }
+
+  }
+
+
+
 
 }
